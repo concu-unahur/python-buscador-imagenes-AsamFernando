aae584db8a1ada00dbd235d66d25f6771c3d58ef
AsamFernando <ferasam88@gmail.com>
2020-02-18 21:02:13 -0300

    bajada de imgs con threads

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 import logging
 from api import PixabayAPI
+import threading
 
 logging.basicConfig(format='%(asctime)s.%(msecs)03d [%(threadName)s] - %(message)s', datefmt='%H:%M:%S', level=logging.INFO)
 
+cantImgs=5
+
 carpeta_imagenes = './imagenes'
-query = 'computadoras'
-api = PixabayAPI('PONER ACÁ EL API KEY', carpeta_imagenes)
+query = 'perro'
+api = PixabayAPI('15310722-6cbef7cd9b9a485bd996b4ad8', carpeta_imagenes)
 
 logging.info(f'Buscando imagenes de {query}')
-urls = api.buscar_imagenes(query, 5)
+urls = api.buscar_imagenes(query, cantImgs)
+
+listaT=[]
 
 for u in urls:
+  listaT.append(threading.Thread(target=api.descargar_imagen, args=[u]))
+
+for t in listaT:
   logging.info(f'Descargando {u}')
-  api.descargar_imagen(u)
+  t.start()
+
+'''for u in urls:
+  logging.info(f'Descargando {u}')
+  api.descargar_imagen(u)'''
